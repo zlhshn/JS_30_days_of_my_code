9bc8c25f2ce4985edf3863bf94f537cb3e5202d6
zlhshn <zsahin4401@gmail.com>
2023-10-02 10:00:01 +0300

    first commit

---
+++ day_01/apps.js
@@ -0,0 +1,43 @@
+console.log('Hello,World!');
+
+// console.log('Welcome to 30DaysofJavaScript');
+
+// let firstName ='Engin'
+// let lastName = 'caglar'
+// let country = 'Türkiye'
+// let age = 35
+// let isMarried = true
+
+// console.log(typeof city);
+// console.log(isMarried,typeof isMarried);
+
+// console.log(`Ad:${firstName} 
+// Soyad:${lastName}
+// Ülke:${country}
+// Yaş:${age}`);
+
+// console.log(firstName,lastName,country,age);
+
+
+// const gravity = 9.81
+// const PI = 3.14
+
+let x= 6,
+  y=7,
+  z= 8;
+
+  console.log(x,y,z);
+
+x = 7
+console.log(x);
+
+const g = 1993
+
+{
+let x = 58
+// console.log(x);
+const g
+
+}
+console.log(x);
+
