c4a5a17b054d237bbe9938d000e818ab4b11c89a
zlhshn <zsahin4401@gmail.com>
2023-10-03 15:30:21 +0300

    day2

---
+++ day_02/apps.js
@@ -0,0 +1,9 @@
+
+// ! driver licence
+
+// !driver licence
+
+const age= Number(prompt("Enter your age:"))
+
+ if(age>=18){console.log("You are old enough to drive.");
+}else{console.log(`You are left with ${18-age} years to drive.`)}
