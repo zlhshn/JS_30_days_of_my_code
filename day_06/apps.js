ee3033cdc7414d6ea44601aad3af474ed93bad7f
zlhshn <zsahin4401@gmail.com>
2023-10-05 16:44:54 +0300

    loop

--- day_06/apps.js
+++ day_06/apps.js
@@ -4,7 +4,7 @@
 // }
 
 
-// for(let i = 0; i <= 7; i++){
+// for(let i = 0; i <= 7; ++i){
 //     console.log(`${i} * ${i} = ${i*i}`) ;
 // }
 
@@ -22,21 +22,21 @@
 
 
 
-const oddOrEven=[10,11,12,13,14,15,16,17,18,19,20,21]
-let even=[]
-let odd=[]
-let sum =0
-let sumo =0
-for(let i=0;i<oddOrEven.length;i++){
-    if(oddOrEven[i]%2==0){
-       even.push(oddOrEven[i])
-       sum = sum + oddOrEven[i]
+// const oddOrEven=[10,11,12,13,14,15,16,17,18,19,20,21]
+// let even=[]
+// let odd=[]
+// let sum =0
+// let sumo =0
+// for(let i=0;i<oddOrEven.length;i++){
+//     if(oddOrEven[i]%2==0){
+//        even.push(oddOrEven[i])
+//        sum = sum + oddOrEven[i]
        
-    }else{odd.push(oddOrEven[i])
-        sumo = sum + oddOrEven[i]
-    }
-}
-console.log(`cift sayilar: ${even},
-bu sayilarin toplami: ${sum}`);
-console.log(`tek sayilar: ${odd},
-bu sayilarin toplami: ${sumo}`);
+//     }else{odd.push(oddOrEven[i])
+//         sumo = sum + oddOrEven[i]
+//     }
+// }
+// console.log(`cift sayilar: ${even},
+// bu sayilarin toplami: ${sum}`);
+// console.log(`tek sayilar: ${odd},
+// bu sayilarin toplami: ${sumo}`);
