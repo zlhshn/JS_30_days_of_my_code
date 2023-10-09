08cda66e2ff8e1ef659cdee35d8d41d4fb574cad
zlhshn <zsahin4401@gmail.com>
2023-10-09 15:20:02 +0300

    day7

--- day_02/apps.js
+++ day_02/apps.js
@@ -24,10 +24,78 @@
 // let string = '30 '
 // console.log(string.concat('girl ', 'and ', 'boy '));
 
-// ?trim()
+// // ?trim()
 
 
-let string  = '        string    '
+// let string  = '        string    '
 
-console.log(string.trim());
+// console.log(string.trim());
 
+// let nums = [1,2,4,9]
+
+// let numbers  = [1,2,4,9]
+
+// console.log(nums === numbers);
+// const PI = Math.PI
+
+// console.log(Math.round(PI));
+// console.log(Math.ceil(PI));
+// console.log(Math.floor(PI));
+// console.log(Math.trunc(PI));
+
+// console.log(Math.max(2,345,567,2,-8));
+// console.log(Math.min(2,345,567,2,-8));
+
+// console.log(Math.abs(89));
+// console.log(Math.sqrt(90));
+// console.log(Math.pow(8,3));
+
+
+// Logarithm
+// Returns the natural logarithm with base E of x, Math.log(x)
+// console.log(Math.log(2))        // 0.6931471805599453
+// console.log(Math.log(10))       // 2.302585092994046
+
+// Returns the natural logarithm of 2 and 10 respectively
+// console.log(Math.LN2)           // 0.6931471805599453
+// console.log(Math.LN10)          // 2.302585092994046
+
+// Trigonometry
+// console.log((Math.sin(0)));
+// Math.sin(60)
+
+// Math.cos(0)
+// Math.cos(60)
+
+
+// ? String
+// let quotWithBackTick = `The saying,'Seeing is Believing' is not correct in 2020.`
+
+// console.log(quotWithBackTick);
+
+// console.log('this is a backslahs\\');
+
+// let string = '       Java   Script      '
+// console.log(string[5]);
+
+// console.log(string[string.length -1]);
+
+// let upper = string.toUpperCase();
+
+// console.log(upper);
+
+// console.log(string.substr(3,4));
+// console.log(string.substring(3,4));
+// console.log(string.split('a'));
+// console.log(string.trim());
+
+
+// let string = '30 Days Of JavaScript'
+
+// let string = 'I Love JavaScript. 209 If you do not love JavaScript what else can you love.'
+// console.log(string.match(/\d+/g))   
+
+// console.log(string.indexOf(6));
+// console.log(string[string.length -1]);
+
+// 
---
+++ day_07/apps.js
@@ -0,0 +1,61 @@
+
+// function printFullName(){
+//     let firstName ='Zeliha'
+//     let lastName = 'Sahin'
+//     let space = ' '
+//     let fullName = firstName + space + lastName 
+//    console.log(fullName);
+// }
+
+// let city = ' Ankara'
+
+// console.log(printFullName() + city);
+
+// printFullName() + city;
+
+
+// function addTwoNumbers(){
+//     let num1 = +prompt('enter a number')
+//     let num2 = +prompt('enter a number')
+//     return num1 + num2
+// }
+
+// console.log((addTwoNumbers()));
+
+
+// function square(num){
+// let area = Math.PI*num*num
+// return area
+// }
+
+// function sumTwoNumbers(num1,num2){
+//     let sum = num1+num2
+//    return sum
+// }
+
+// console.log(sumTwoNumbers(4,6));
+
+// function printFullName(firstName, lastName){
+//     return `${firstName} ${lastName}`
+// }
+
+// console.log(printFullName('zeliha','sahin'));
+
+// document.write(printFullName('zeliha','sahin'))
+
+
+
+// function sumArrayValues(arr){
+//     let sum = 0 
+//     for (let i= 0; i< =arr.l){
+    
+//         sum = sum + arr[i]
+//     }
+//     return sum;
+// }
+// const numbers = [ 1,2,3,4 ]
+// console.log(sumArrayValues(numbers));
+
+// const areaOfCircle = (radius) => {
+//     let area = Math.
+// }
