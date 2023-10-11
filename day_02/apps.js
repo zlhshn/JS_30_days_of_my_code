f2017a4856e57d90f9bf222a9e4a1004759a3016
zlhshn <zsahin4401@gmail.com>
2023-10-11 15:52:46 +0300

    logical

--- day_02/apps.js
+++ day_02/apps.js
@@ -200,7 +200,7 @@
 
 // ! Level - 3 3.soru yarım kaldı
 
-let sentence = '%I $am@% a %tea@cher%, &and& I lo%#ve %te@a@ching%;. The@re $is no@th@ing; &as& mo@re rewarding as educa@ting &and& @emp%o@weri@ng peo@ple. ;I found tea@ching m%o@re interesting tha@n any ot#her %jo@bs. %Do@es thi%s mo@tiv#ate yo@u to be a tea@cher!? %Th#is 30#Days&OfJavaScript &is al@so $the $resu@lt of &love& of tea&ching'
+// let sentence = '%I $am@% a %tea@cher%, &and& I lo%#ve %te@a@ching%;. The@re $is no@th@ing; &as& mo@re rewarding as educa@ting &and& @emp%o@weri@ng peo@ple. ;I found tea@ching m%o@re interesting tha@n any ot#her %jo@bs. %Do@es thi%s mo@tiv#ate yo@u to be a tea@cher!? %Th#is 30#Days&OfJavaScript &is al@so $the $resu@lt of &love& of tea&ching'
 
 // sentence = sentence.replace(/@/g,'')
 // sentence = sentence.replace(/%/g,'')
@@ -218,6 +218,6 @@ let sentence = '%I $am@% a %tea@cher%, &and& I lo%#ve %te@a@ching%;. The@re $is 
 
 // ?soru -4-eksik
 
-const newSentence ='He earns 5000 euro from salary per month, 10000 euro annual bonus, 15000 euro online courses per month.'
+// const newSentence ='He earns 5000 euro from salary per month, 10000 euro annual bonus, 15000 euro online courses per month.'
 
-console.log(newSentence.match(/\d+/g));
+// console.log(newSentence.match(/\d+/g));
---
+++ day_03/apps3.js
@@ -0,0 +1,61 @@
+
+// * Boolean 
+
+// let isLightOn = true
+// let isRaining = false
+
+// let  falseValue = 4>3
+// console.log(falseValue);
+
+// * Null
+
+// let empty = null
+
+// console.log(typeof empty);
+
+// * Aritmethic Operators
+
+// let num1 = 4
+
+// let num2 = 7
+
+// console.log(`${num1+num2}`);
+
+// console.log(num1-num2);
+// console.log(num1*num2);
+// console.log(num1**num2);
+
+// const PI = 3.14
+// let radius = 100
+
+// let areaOfCircle = PI*radius**2
+
+// console.log(areaOfCircle);
+
+
+// const gravity = 9.81
+
+// let mass = 75
+
+// let weight = mass*gravity
+
+// console.log(weight);
+
+// * Comparison operator
+
+// console.log(3<4);
+// console.log(4===4);
+// console.log('5' === 5);
+// console.log(3 != 7);
+// console.log(3 !== '3');
+// console.log(0 == false);
+// console.log(NaN == NaN);
+// console.log(undefined == undefined);
+// console.log(3 === 3) 
+// console.log('python'.length < 'dragon'.length);
+
+
+// * Ternary Operation
+
+
+
