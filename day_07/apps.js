0dcd82582f8d35084a0b2bbcbf6280d7b799e1ca
zlhshn <zsahin4401@gmail.com>
2023-10-12 23:24:03 +0300

    js

--- day_07/apps.js
+++ day_07/apps.js
@@ -171,6 +171,46 @@
 // *Level 2-----10.Soru
 //* Declare a function name sumOfNumbers. It takes a number parameter and it adds all the numbers in that range.
 
+// const sumOfNumbers = function(){
+//     let sum = 0
+//     for(let i =0; i<arguments.length;i++){
+//         sum += arguments[i]
+//     }
+
+//     return sum
+// }
+
+// console.log(sumOfNumbers(2,6,4));
+
+
+
+// const  sumOfNumber = (...numbers) =>{
+//     sum= 0
+//     for(const element of numbers){
+
+//         sum += element
+//     }
+
+//     return sum
+
+// }
+
+
+// console.log(sumOfNumber(6,5,2));
+
+
+
+
+// const top = function(x,y){
+
+//     for(i = x ; )
+
+
+
+// }
+
+
+
 
 // 1.yöntem
 //  const sumOfNumbers =function (){
@@ -187,7 +227,7 @@
 // const sumOfNumber = (...args) =>{
 //   sum = 0
 
-// // const element of args
+// const element of args
 
 //   for(let i = 0; i<args.length;i++){   
 //     sum += args[i]
@@ -201,17 +241,19 @@
 // *Declare a function name sumOfOdds. It takes a number parameter and it adds all the odd numbers in that  range.
 
 
+
 // const sumOfOdds = function(){
 //   let sum_odd = 0
+//   let sum_even = 0
 //   for(let i = 0; i<arguments.length;i++){
    
-//     arguments[i] %2 ? sum_odd +=arguments[i] : ''
+//     arguments[i] %2 == 0 ? sum_even +=arguments[i] : sum_odd += arguments[i]
 //   }
-//     return sum_odd
+//     return `${sum_even}  , ${sum_odd}`
   
 // }
 
-// console.log(sumOfOdds(1,2,9,6,4));
+// console.log(sumOfOdds(1,2,9,6,3));
 
 
 
@@ -241,7 +283,7 @@
 // *Level-3----15.soru
 // *Write a function called isPrime, which checks if a number is prime number.
 
-// 1.çözüm
+
 // const isPrime = function(){
 
 //  let a=Number(prompt("Asal olup olmadiğini öğrenmek istediğiniz sayiyi giriniz:")) ;
@@ -280,3 +322,43 @@
 
 // console.log(unique(3,'dizi',9,7,'g',7));
 
+
+
+
+// ! renk üreten kod
+// function generateColors(type) {
+//     if (type === "hex") {
+//       return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+//     } else if (type === "rgb") {
+//       const randomValue = () => Math.floor(Math.random() * 256);
+//       return `rgb(${randomValue()}, ${randomValue()}, ${randomValue()})`;
+//     }
+//   }
+  
+//   const hexColor = generateColors("hex");
+//   console.log("Rastgele Hex Renk:", hexColor);
+  
+//   const rgbColor = generateColors("rgb");
+//   console.log("Rastgele RGB Renk:", rgbColor);
+
+
+
+
+
+const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789><.£#$!';
+function generateRandomCode(hane){
+    
+   
+    let result = ""
+    let charactersLength = characters.length;
+    for ( let i = 0; i < hane ; i++ ) {
+        result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result
+
+
+}
+const hane = Number(prompt("kaç haneli şifre istersin?"))
+// const kere = Number(prompt("kaç farklı şifre istersin?"))
+
+console.log(generateRandomCode(hane));
