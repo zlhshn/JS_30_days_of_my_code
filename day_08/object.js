0c74d0597e2618eca9ae299a8f1b1fa4423d6d46
zlhshn <zsahin4401@gmail.com>
2023-10-15 16:20:13 +0300

    object start

---
+++ day_08/object.js
@@ -0,0 +1,14 @@
+
+const person = {
+    firstName : 'Zeliha',
+    lastName :'Sahin',
+    age : 32,
+    country :'Turkey',
+    city:'Ankara',
+}
+console.log(person);
+
+
+console.log(person.firstName);
+
+console.log(person['firstName']);
