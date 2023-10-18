4fb8e25f774357d6ff233a7c80503cdbdbe34370
zlhshn <zsahin4401@gmail.com>
2023-10-18 15:58:35 +0300

    obj

--- day_08/object.js
+++ day_08/object.js
@@ -1,17 +1,17 @@
 
-const person = {
-    firstName : 'Zeliha',
-    lastName :'Sahin',
-    age : 32,
-    country :'Turkey',
-    city:'Ankara',
-}
-console.log(person);
+// const person = {
+//     firstName : 'Zeliha',
+//     lastName :'Sahin',
+//     age : 32,
+//     country :'Turkey',
+//     city:'Ankara',
+// }
+// console.log(person);
 
 
-console.log(person.firstName);
+// console.log(person.firstName);
 
-console.log(person['firstName']);
+// console.log(person['firstName']);
 
 
 // ! Level-1----------------
@@ -43,79 +43,79 @@ console.log(person['firstName']);
 
 // ! Level----2----
 
-const users = {
-  Alex: {
-    email: "alex@alex.com",
-    skills: ["HTML", "CSS", "JavaScript"],
-    age: 20,
-    isLoggedIn: false,
-    points: 30,
-  },
-  Asab: {
-    email: "asab@asab.com",
-    skills: [
-      "HTML",
-      "CSS",
-      "JavaScript",
-      "Redux",
-      "MongoDB",
-      "Express",
-      "React",
-      "Node",
-    ],
-    age: 25,
-    isLoggedIn: false,
-    points: 50,
-  },
-  Brook: {
-    email: "daniel@daniel.com",
-    skills: ["HTML", "CSS", "JavaScript", "React", "Redux"],
-    age: 30,
-    isLoggedIn: true,
-    points: 50,
-  },
-  Daniel: {
-    email: "daniel@alex.com",
-    skills: ["HTML", "CSS", "JavaScript", "Python"],
-    age: 20,
-    isLoggedIn: false,
-    points: 40,
-  },
-  John: {
-    email: "john@john.com",
-    skills: ["HTML", "CSS", "JavaScript", "React", "Redux", "Node.js"],
-    age: 20,
-    isLoggedIn: true,
-    points: 50,
-  },
-  Thomas: {
-    email: "thomas@thomas.com",
-    skills: ["HTML", "CSS", "JavaScript", "React"],
-    age: 20,
-    isLoggedIn: false,
-    points: 40,
-  },
-  Paul: {
-    email: "paul@paul.com",
-    skills: [
-      "HTML",
-      "CSS",
-      "JavaScript",
-      "MongoDB",
-      "Express",
-      "React",
-      "Node",
-    ],
-    age: 20,
-    isLoggedIn: false,
-    points: 40,
-  },
-};
+// const users = {
+//   Alex: {
+//     email: "alex@alex.com",
+//     skills: ["HTML", "CSS", "JavaScript"],
+//     age: 20,
+//     isLoggedIn: false,
+//     points: 30,
+//   },
+//   Asab: {
+//     email: "asab@asab.com",
+//     skills: [
+//       "HTML",
+//       "CSS",
+//       "JavaScript",
+//       "Redux",
+//       "MongoDB",
+//       "Express",
+//       "React",
+//       "Node",
+//     ],
+//     age: 25,
+//     isLoggedIn: false,
+//     points: 50,
+//   },
+//   Brook: {
+//     email: "daniel@daniel.com",
+//     skills: ["HTML", "CSS", "JavaScript", "React", "Redux"],
+//     age: 30,
+//     isLoggedIn: true,
+//     points: 50,
+//   },
+//   Daniel: {
+//     email: "daniel@alex.com",
+//     skills: ["HTML", "CSS", "JavaScript", "Python"],
+//     age: 20,
+//     isLoggedIn: false,
+//     points: 40,
+//   },
+//   John: {
+//     email: "john@john.com",
+//     skills: ["HTML", "CSS", "JavaScript", "React", "Redux", "Node.js"],
+//     age: 20,
+//     isLoggedIn: true,
+//     points: 50,
+//   },
+//   Thomas: {
+//     email: "thomas@thomas.com",
+//     skills: ["HTML", "CSS", "JavaScript", "React"],
+//     age: 20,
+//     isLoggedIn: false,
+//     points: 40,
+//   },
+//   Paul: {
+//     email: "paul@paul.com",
+//     skills: [
+//       "HTML",
+//       "CSS",
+//       "JavaScript",
+//       "MongoDB",
+//       "Express",
+//       "React",
+//       "Node",
+//     ],
+//     age: 20,
+//     isLoggedIn: false,
+//     points: 40,
+//   },
+// };
 
-let winner;
-let max = 0;
+// let winner;
+// let max = 0;
 
-console.log();
+// console.log();
 
 // for (const property in users) {
 //   if (users[property].skills.length > max) {
