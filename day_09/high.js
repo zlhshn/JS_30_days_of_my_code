6840e7a16c74480d49820671f729d242ecc5bf8b
zlhshn <zsahin4401@gmail.com>
2023-10-19 13:13:22 +0300

    high

---
+++ day_09/high.js
@@ -0,0 +1,12 @@
+// const higherOrder = n =>{
+//     const doSomeThing = m =>{
+//         const doWhatEver= t =>{
+//             return 2* n +3 * m +t
+//         }
+//         return doWhatEver
+//     }
+//     return doSomeThing
+// }
+
+// console.log(higherOrder(2)(3)(10));
+
