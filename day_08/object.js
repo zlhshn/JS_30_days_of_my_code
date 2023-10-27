a37b23fb1d35873111de405aa281fe16989d0372
zlhshn <zsahin4401@gmail.com>
2023-10-27 12:37:33 +0300

    solution

--- day_08/object.js
+++ day_08/object.js
@@ -144,100 +144,69 @@
 
 // ! level--3
 
-const personAccount = {}
-
-personAccount.firstname = 'zeliha'
-personAccount.lastName = 'sahin'
-personAccount.incomes = "0$"
-personAccount.expenses = "250$" 
-personAccount.accountInfo=  function(){
-      return  ` ${this.incomes} - ${this.expenses}`
-}
-
-
-console.log(personAccount.accountInfo());
-console.log(Object.entries(personAccount));
-console.log(Object.keys(personAccount));
-console.log(Object.values(personAccount));
-
-  
-personAccount.totalIncome = function(){
-    let tincome = 0
-    let values = this.incomes
-}
 
 
 
-const users = [
-    {
-        _id: 'ab12ex',
-        username: 'Alex',
-        email: 'alex@alex.com',
-        password: '123123',
-        createdAt:'08/01/2020 9:00 AM',
-        isLoggedIn: false
-    },
-    {
-        _id: 'fg12cy',
-        username: 'Asab',
-        email: 'asab@asab.com',
-        password: '123456',
-        createdAt:'08/01/2020 9:30 AM',
-        isLoggedIn: true
-    },
-    {
-        _id: 'zwf8md',
-        username: 'Brook',
-        email: 'brook@brook.com',
-        password: '123111',
-        createdAt:'08/01/2020 9:45 AM',
-        isLoggedIn: true
-    },
-    {
-        _id: 'eefamr',
-        username: 'Martha',
-        email: 'martha@martha.com',
-        password: '123222',
-        createdAt:'08/01/2020 9:50 AM',
-        isLoggedIn: false
-    },
-    {
-        _id: 'ghderc',
-        username: 'Thomas',
-        email: 'thomas@thomas.com',
-        password: '123333',
-        createdAt:'08/01/2020 10:00 AM',
-        isLoggedIn: false
-    }
-    ];
 
-    const products = [
-  {
-    _id: 'eedfcf',
-    name: 'mobile phone',
-    description: 'Huawei Honor',
-    price: 200,
-    ratings: [
-      { userId: 'fg12cy', rate: 5 },
-      { userId: 'zwf8md', rate: 4.5 }
-    ],
-    likes: []
+
+const users = {
+  Alex: {
+    email: 'alex@alex.com',
+    skills: ['HTML', 'CSS', 'JavaScript'],
+    age: 20,
+    isLoggedIn: false,
+    points: 30
   },
-  {
-    _id: 'aegfal',
-    name: 'Laptop',
-    description: 'MacPro: System Darwin',
-    price: 2500,
-    ratings: [],
-    likes: ['fg12cy']
+  Asab: {
+    email: 'asab@asab.com',
+    skills: ['HTML', 'CSS', 'JavaScript', 'Redux', 'MongoDB', 'Express', 'React', 'Node'],
+    age: 25,
+    isLoggedIn: false,
+    points: 50
   },
-  {
-    _id: 'hedfcg',
-    name: 'TV',
-    description: 'Smart TV:Procaster',
-    price: 400,
-    ratings: [{ userId: 'fg12cy', rate: 5 }],
-    likes: ['fg12cy']
+  Brook: {
+    email: 'daniel@daniel.com',
+    skills: ['HTML', 'CSS', 'JavaScript', 'React', 'Redux'],
+    age: 30,
+    isLoggedIn: true,
+    points: 50
+  },
+  Daniel: {
+    email: 'daniel@alex.com',
+    skills: ['HTML', 'CSS', 'JavaScript', 'Python'],
+    age: 20,
+    isLoggedIn: false,
+    points: 40
+  },
+  John: {
+    email: 'john@john.com',
+    skills: ['HTML', 'CSS', 'JavaScript', 'React', 'Redux', 'Node.js'],
+    age: 20,
+    isLoggedIn: true,
+    points: 50
+  },
+  Thomas: {
+    email: 'thomas@thomas.com',
+    skills: ['HTML', 'CSS', 'JavaScript', 'React'],
+    age: 20,
+    isLoggedIn: false,
+    points: 40
+  },
+  Paul: {
+    email: 'paul@paul.com',
+    skills: ['HTML', 'CSS', 'JavaScript', 'MongoDB', 'Express', 'React', 'Node'],
+    age: 20,
+    isLoggedIn: false,
+    points: 40
   }
-]
+}
 
+for(const user in users){
+ 
+  if(user.skills === 'MongoDB' && 'Express' && 'React' && 'Node')
+        {console.log(user);
+        }
+      
+  
+  
+}
--- day_11/destructing.js
+++ day_11/destructing.js
@@ -153,55 +153,306 @@
 
 // ! level-2
 
+// const users = [
+//   {
+//     name: "Brook",
+//     scores: 75,
+//     skills: ["HTM", "CSS", "JS"],
+//     age: 16,
+//   },
+//   {
+//     name: "Alex",
+//     scores: 80,
+//     skills: ["HTM", "CSS", "JS"],
+//     age: 18,
+//   },
+//   {
+//     name: "David",
+//     scores: 75,
+//     skills: ["HTM", "CSS"],
+//     age: 22,
+//   },
+//   {
+//     name: "John",
+//     scores: 85,
+//     skills: ["HTML"],
+//     age: 25,
+//   },
+//   {
+//     name: "Sara",
+//     scores: 95,
+//     skills: ["HTM", "CSS", "JS"],
+//     age: 26,
+//   },
+//   {
+//     name: "Martha",
+//     scores: 80,
+//     skills: ["HTM", "CSS", "JS"],
+//     age: 18,
+//   },
+//   {
+//     name: "Thomas",
+//     scores: 90,
+//     skills: ["HTM", "CSS", "JS"],
+//     age: 20,
+//   },
+// ];
+
+// users.forEach((element) => {
+//   const { name, scores, skills, age } = element;
+//   console.log(name);
+//   console.log(scores);
+//   console.log(skills);
+//   console.log(age);
+// });
+
+
+// const filtered = users
+// .filter(user=> user.skills.length <2)
+// .forEach((x)=> {
+//   const {name} = x
+//   console.log(name);
+// })
+
+
+
+// for ({
+//   name,
+//   skills,
+// } of users) {
+// if (skills.length < 2) {
+//   console.log(name, skills)
+// }
+// }
+
+
+// ! Level--3
+
+// const student = ['David', ['HTM', 'CSS', 'JS', 'React'], [98, 85, 90, 95]]
+
+// const [name,skills,[a,b,...rest]] = student
+
+// console.log(name1, skills, rest)
+
+// const students = [
+//   ['David', ['HTM', 'CSS', 'JS', 'React'],
+//       [98, 85, 90, 95]
+//   ],
+//   ['John', ['HTM', 'CSS', 'JS', 'React'],
+//       [85, 80, 85, 80]
+//   ]
+// ]
+
+
+// const convertArrayToObject=(students) =>{
+//    for(const [name2,skills,score] of students){
+//     return {name2,skills,score}
+//    }
+  
+// }
+
+// console.log(convertArrayToObject(students));
+
+// const student = {
+//   name: 'David',
+//   age: 25,
+//   skills: {
+//     frontEnd: [
+//       { skill: 'HTML', level: 10 },
+//       { skill: 'CSS', level: 8 },
+//       { skill: 'JS', level: 8 },
+//       { skill: 'React', level: 9 }
+//     ],
+//     backEnd: [
+//       { skill: 'Node',level: 7 },
+//       { skill: 'GraphQL', level: 8 },
+//     ],
+//     dataBase:[
+//       { skill: 'MongoDB', level: 7.5 },
+//     ],
+//     dataScience:['Python', 'R', 'D3.js']
+//   }
+// }
+
+// const newStudent = {...student}
+
+// newStudent.skills.frontEnd.push({skill:'Bootstrap',level:8})
+// newStudent.skills.backEnd.push({skill:'Express',level:9})
+// newStudent.skills.dataBase.push({skill:'SQL',level:8})
+// newStudent.skills.dataScience.push('SQL')
+
+// console.log(newStudent);
+
+
+const constants = [2.72, 3.14, 9.81, 37, 100]
+
+const [e, pi, gravity, humanBodyTemp, waterBoilingTemp] = constants
+
+console.log(gravity);
+
+console.log(constants[2]) 
+
+const countries = ['Finland', 'Estonia', 'Sweden', 'Denmark', 'Norway']
+
+const [fin, est, sw, den, nor] = countries
+
+console.log(fin);
+console.log(den);
+
+
+const rectangle = {
+  width: 20,
+  height: 10,
+  area: 200,
+  perimeter: 60
+}
+
+const {height:h,width,area,} = rectangle
+
+console.log(h);
+console.log(width);
+
 const users = [
   {
-    name: "Brook",
-    scores: 75,
-    skills: ["HTM", "CSS", "JS"],
-    age: 16,
+    name:'Brook',
+    scores:75,
+    skills:['HTM', 'CSS', 'JS'],
+    age:16
   },
   {
-    name: "Alex",
-    scores: 80,
-    skills: ["HTM", "CSS", "JS"],
-    age: 18,
+    name:'Alex',
+    scores:80,
+    skills:['HTM', 'CSS', 'JS'],
+    age:18
   },
   {
-    name: "David",
-    scores: 75,
-    skills: ["HTM", "CSS"],
-    age: 22,
+    name:'David',
+    scores:75,
+    skills:['HTM', 'CSS'],
+    age:22
   },
   {
-    name: "John",
-    scores: 85,
-    skills: ["HTML"],
-    age: 25,
+    name:'John',
+    scores:85,
+    skills:['HTML'],
+    age:25
   },
   {
-    name: "Sara",
-    scores: 95,
-    skills: ["HTM", "CSS", "JS"],
-    age: 26,
+    name:'Sara',
+    scores:95,
+    skills:['HTM', 'CSS', 'JS'],
+    age: 26
   },
   {
-    name: "Martha",
-    scores: 80,
-    skills: ["HTM", "CSS", "JS"],
-    age: 18,
+    name:'Martha',
+    scores:80,
+    skills:['HTM', 'CSS', 'JS'],
+    age:18
   },
   {
-    name: "Thomas",
-    scores: 90,
-    skills: ["HTM", "CSS", "JS"],
-    age: 20,
-  },
-];
+    name:'Thomas',
+    scores:90,
+    skills:['HTM', 'CSS', 'JS'],
+    age:20
+  }
+  ]
 
-users.forEach((element) => {
-  const { name, scores, skills, age } = element;
-  console.log(name);
-  console.log(scores);
-  console.log(skills);
-  console.log(age);
-});
+//  for( const {name,scores,skills,age} of users){
+
+//         console.log(name);
+//         console.log(scores);
+//  }
+
+
+// const filteredSkill = users.filter((s)=> s.skills.length<2).forEach((n)=>console.log(n.name))
+
+
+
+// const student = ['David', ['HTM', 'CSS', 'JS', 'React'], [98, 85, 90, 95]]
+
+// const [name,skills,[a1,...kalan] ] = student
+
+// // console.log(name, skills, jsScore, reactScore)
+
+// console.log(name, skills,kalan)
+
+
+// const students = [
+//   ['David', ['HTM', 'CSS', 'JS', 'React'], [98, 85, 90, 95]],
+//   ['John', ['HTM', 'CSS', 'JS', 'React'], [85, 80, 85, 80]]
+// ]
+
+// const convertArrayToObject= (students)=>{
+   
+//    const news1 = students.map((s)=> ({name:s[0],skills:s[1],scores:s[2]}))
+
+//    return news1
+     
+// }
+// console.log(convertArrayToObject(students));
+
+
+
+// const convertArrayToObject1 = (arr) => {
+//   let arrOfObjects = [];
+//   for(const [name, skills, scores] of arr) 
+//       arrOfObjects.push({name, skills, scores});
+//   return arrOfObjects;
+// } 
+
+// console.log(convertArrayToObject1(students));
+
+const student = {
+  name: 'David',
+  age: 25,
+  skills: {
+    frontEnd: [
+      { skill: 'HTML', level: 10 },
+      { skill: 'CSS', level: 8 },
+      { skill: 'JS', level: 8 },
+      { skill: 'React', level: 9 }
+    ],
+    backEnd: [
+      { skill: 'Node',level: 7 },
+      { skill: 'GraphQL', level: 8 },
+    ],
+    dataBase:[
+      { skill: 'MongoDB', level: 7.5 },
+    ],
+    dataScience:['Python', 'R', 'D3.js']
+  }
+}
+
+
+const newStudent2 = {...student}
+console.log(newStudent2);
+
+
+
+newStudent2.skills.frontEnd.push({skill:'Bootstrap',level:8})
+newStudent2.skills.backEnd.push({skill:'Express',level:9})
+
+
+const newStudent = {
+  ...student
+  ,skills: {
+    ...student.skills,
+    frontEnd: [...student.skills.frontEnd, { skill: "BootStrap", level: 8 }],
+    backEnd: [...student.skills.backEnd, { skill: "Express", level: 9 }],
+    dataBase: [...student.skills.dataBase, { skill: "SQL", level: 8 }],
+    dataScience: [...student.skills.dataScience, "SQL"],
+  },
+};
+
+console.log(newStudent);
+
+
+// console.log(newStudent2);
+// const newstudent1 = student
+// newstudent1.age = 39
+// console.log(newstudent1);
+
+
+
+
+// console.log(student);
